39e2966fc864a25c14be9de8dd97d54706073519
adhi0331 <adhi03.ananthan@gmail.com>
2022-07-09 09:51:20 -0700

    Added new content

---
+++ jsbasics/arrays/index.js
@@ -0,0 +1,13 @@
+var foods = ["Apple", "Pizza", "Noodles", "Bread"];
+
+//foods.push("Orange")
+//foods.pop()
+//foods.indexOf("Apple")
+//foods.splice(1)
+
+//var newfoods = foods.map(x => x + " is yummy")
+//console.log(newfoods)
+
+/* create a new array that multiples all elements in nums by 3 using the map function*/
+
+//var nums = [1, 2, 3, 4, 5]
---
+++ jsbasics/variables&datatypes/index.js
@@ -0,0 +1,26 @@
+/* Variables */
+let lname = "Add your name"
+var vname = "Add your name"
+const cname = "Add your name" //Cannot be redeclared
+
+/* Data Types */
+
+var number = 19 //numbers
+var x //variable
+var name = "Your Name" //string
+var boolean = true //booleans
+
+/* Objects */
+
+var hobbies = {
+    sport: "Add your favorite sport",
+    music: "Add your favorite music genre",
+    shoe: "Add your favorite shoe"
+}
+
+
+
+
+
+
+
