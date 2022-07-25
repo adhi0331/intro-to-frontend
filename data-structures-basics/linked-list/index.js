572b4003ff8d1f2830e0059c8b313d3008569045
adhi0331 <adhi03.ananthan@gmail.com>
2022-07-25 11:15:42 -0700

    added starter code for linked list

---
+++ data-structures-basics/linked-list/index.js
@@ -0,0 +1,68 @@
+class LinkedList {
+    constructor(head = null){
+        this.head = head
+        this.size = 1
+    }
+
+    add(index, data) {
+        if(index < 0 || index > this.size || data == null){
+            throw "Invalid arguments"
+        }
+
+        let newNode = new ListNode(data)
+        let currNode = this.head
+        let curIndex = 0
+        while(curIndex!=index){
+            currNode = currNode.getNext()
+            curIndex++
+        }
+        newNode.setNext(currNode.getNext)
+        currNode.setNext(newNode)
+        this.size++;
+    }
+
+    append(data) {
+        this.add(this.size, data)
+    }
+
+    //Finish method to remove elements in the linked list
+    remove(index) {
+        if(index<0 || index>=size) {
+            throw "Invalid arguments"
+        }
+    }
+
+
+}
+
+class ListNode {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+
+    setNext(next){
+        this.next = next
+    }
+
+    setElement(data) {
+        this.data = data
+    }
+
+    getNext() {
+        return this.next
+    }
+
+    getElement() {
+        return this.data
+    }
+}
+
+let node1 = new ListNode(1)
+let node2 = new ListNode(2)
+node1.next = node2
+
+console.log(node1.next)
+let list = new LinkedList(node1)
+list.append(3)
+console.log(list.head.next.next.data)
